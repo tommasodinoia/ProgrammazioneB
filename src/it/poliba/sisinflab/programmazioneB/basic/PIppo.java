21afff87cd8822abed14a7576562be7e014817ad
Tommaso Di Noia <tommaso.dinoia@gmail.com>
2025-03-12 19:14:27 +0100

    Delete src/it/poliba/sisinflab/programmazioneB/basic/PIppo.java
    Errore

--- src/it/poliba/sisinflab/programmazioneB/basic/PIppo.java
+++
@@ -1,8 +0,0 @@
-package it.poliba.sisinflab.programmazioneB.basic;
-
-public class PIppo {
-    public static void main(String[] args) {
-        ReturnedBinary day = ReturnedBinary.ITERATION;
-        System.out.println(day);
-    }
-}
